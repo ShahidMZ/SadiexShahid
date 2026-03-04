42cbb372aa38c7daba283459dad44b8cff20c9d4
ShahidMZ <shahidmzubair@gmail.com>
2026-03-04 16:42:20 -0600

    Updated landing page

--- src/features/home/home.ts
+++ src/features/home/home.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-home',
-    imports: [],
+    imports: [CommonModule, FormsModule],
     templateUrl: './home.html',
     styleUrl: './home.css',
 })
 export class Home {
+    protected password = signal("");
+    protected allowAccess = signal(false);
+    private correctPassword = "SSDM2026%!";
     
+    login() {
+        if (this.password() == this.correctPassword) {
+            this.allowAccess.set(true);
+        }
+    }
 }
